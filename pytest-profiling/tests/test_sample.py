fef21614f65b29e1970d0a9d4a37b56cc6a2c9b7
kishimoto.ryo <kishimoto.ryo@mail.canon>
2020-04-02 20:04:08 +0900

    [fix] まともに動かないテストを変更

--- pytest-profiling/tests/test_sample.py
+++ pytest-profiling/tests/test_sample.py
@@ -3,47 +3,33 @@ import pytest
 import sys
 
 
+COUNT = 10000000
+
 @pytest.fixture(scope='module')
 def expected():
-  return [i for i in range(1, 1000000)]
+  return [i for i in range(1, COUNT)]
 
 
 def test_1_1(expected):
-  actual = [i for i in range(1, 1000000)]
+  actual = [i for i in range(1, COUNT)]
   assert expected == actual
 
+
 def test_1_2(expected):
+  actual = list({i for i in range(1, COUNT)})
+  assert expected == actual
+
+
+def test_1_3(expected):
   actual = []
-  for i in range(1, 1000000):
+  for i in range(1, COUNT):
     actual.append(i)
   assert expected == actual
 
-def test_1_3(expected):
-  def total(n):
-    if n < 1:
-      return []
-    if n < 999:
-      return [i for i in range(1, n)]
-    return total(n - 1000) + [i for i in range(n - 999, n)]
-  actual = total(100000)
-  assert expected == actual
 
-def test_2():
-  expected = [0, 1, 2, 3]
-  actual = [i for i in range(4)]
-  assert expected == actual
-
-def test_3():
-  expected = {'1': 1, '2': 2}
-  actual = {str(i): i for i in range(1,3)}
-  assert expected == actual
-
-def test_4():
-  expected = {0, 1, 2, 3}
-  actual = {i for i in range(4)}
-  assert expected == actual
-
-def test_5():
-  expected = [0, 2, 4, 6, 8]
-  actual = [i for i in range(10) if i % 2 == 0]
+def test_1_4(expected):
+  actual = []
+  for i in range(1, COUNT):
+    # actual = actual + [i] とすると死ぬほど時間かかる
+    actual += [i]
   assert expected == actual
